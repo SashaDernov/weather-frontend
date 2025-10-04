a2588e9e1a81ee67236c78411c90b3d4df9a422e
Alexander Dernov <alexdernov@MacBook-Air-Alexander.local>
2025-10-04 23:36:50 +0300

    ci: add GitHub Actions deploy to Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/weather-frontend/',  // 👈 имя твоего репо
   plugins: [react()],
 })
